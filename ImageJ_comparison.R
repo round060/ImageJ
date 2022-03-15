8216b5fb04ce6b6d71fefd1035ae8edb48fb689d
Christopher Rounds <round060@umn.edu>
2022-03-15 11:50:52 -0500

    changing comparison script

--- ImageJ_comparison.R
+++ ImageJ_comparison.R
@@ -1,10 +1,16 @@
+#remotes::install_github("GLEON/GLM3r")
+#remotes::install_github('usgs-r/glmtools')
+#remotes::install_github("jsta/glmutils")
+library(glmtools)
 library(ggplot2)
 library(mnsentinellakes)
 library(dplyr)
 library(stringr)
 library(SimilarityMeasures)
 library(mosaic)
+
 se <- function(x) sqrt(var(x)/length(x))
+lake_areas <- read.csv("data/MN_areas.csv")
 
 DEM_hypsos <- readRDS("data/MN_bathy.rds")
 DEM_hypsos$DOW <- fixlakeid(DEM_hypsos$DOW)
@@ -77,19 +83,19 @@ All_Hypsos <- rbind(DEM_Hypso_Perc, Perc_Hypsos_CR, Perc_Hypsos_AVP)
 
 Hypsos_depth <- All_Hypsos
 Hypsos_depth$proportion_depth = NA
-maxdepth_lake <- data.frame("dow" = character(1), "max_depth" = numeric(1), 
+maxdepth_lake <- data.frame("DOW" = character(1), "max_depth" = numeric(1), 
                             "method" = character(1), stringsAsFactors = FALSE)
 
 #get the maximum depth for each lake using each method
-max_depth = Hypsos_depth[1,3]; dow = Hypsos_depth[1,2] 
+max_depth = Hypsos_depth[1,3]; DOW = Hypsos_depth[1,2] 
 method = "DEM"; i = 1
 for (row in 1:nrow(Hypsos_depth)) {
-  if (dow != Hypsos_depth[row,2]) {
-    maxdepth_lake[i,1] = dow
+  if (DOW != Hypsos_depth[row,2]) {
+    maxdepth_lake[i,1] = DOW
     maxdepth_lake[i,2] = max_depth
     maxdepth_lake[i,3] = method
     i = i + 1
-    dow = Hypsos_depth[row,2]
+    DOW = Hypsos_depth[row,2]
     max_depth = Hypsos_depth[row,3]
   }
   else {
@@ -97,74 +103,84 @@ for (row in 1:nrow(Hypsos_depth)) {
     max_depth = Hypsos_depth[row,3]
   }
 }
-maxdepth_lake[i,1] = dow; maxdepth_lake[i,2] = max_depth; maxdepth_lake[i,3] = method
+maxdepth_lake[i,1] = DOW; maxdepth_lake[i,2] = max_depth; maxdepth_lake[i,3] = method
 #write.csv(maxdepth_lake, "data/max_depth_validation.csv", row.names = F)
+
+
+### calulate what hypsography would be with only max-depth ###
+#Use package General Lake Model#
+all_hypsos <- subset(All_Hypsos, select = -c(depths))
+#sets up the null models
+null_hypso = data.frame(DOW = integer(), depth_feet = numeric(), proportion_area = numeric(), Method = character())
+null_hypso = null_hypso[-1,]
+max_depth <- maxdepth_lake %>% filter(method == "DEM")
+for (i in 1:nrow(max_depth)) {
+  if (max_depth[i,2] < 10) { #specifies the number of depths to interpolate depth from, more = more measurements
+    nlayers = max_depth[i,2]*2
+  }else{
+    nlayers = round(max_depth[i,2])
+  }
+  res <- suppressWarnings( # does the actual generation of hypsography 
+    glmutils::generate_hypsography(round(nlayers), max_depth[i,2], 1))
+  res$DOW = max_depth[i,1]
+  
+  #Probably the worst way to do this because its running at like n^2 speed
+  null_hypso = cbind(DOW = res[,4], depth_feet = res[,3], proportion_area = res[,1], Method = "ellipsoid")
+  all_hypsos = rbind(all_hypsos, null_hypso)
+  null_hypso = cbind(DOW = res[,4], depth_feet = res[,2], proportion_area = res[,1], Method = "linear") 
+  all_hypsos = rbind(all_hypsos, null_hypso)
+}
+
+#I know im bad
+all_hypsos$depth_feet = as.numeric(all_hypsos$depth_feet)
+all_hypsos$proportion_area = as.numeric(all_hypsos$proportion_area)
 
 
 ### CALCULATE AVERAGE VERTICAL DISTANCE BETWEEN THE CURVES ####
 
 
-## Difference between hypsos Chris did and DEM
-avg.abs.diff <- rep(NA, length(Perc_DOWs_CR))
-root.meansq.diff <- rep(NA, length(Perc_DOWs_CR))
-avg.ImageJ.DEM.diff <- rep(NA, length(Perc_DOWs_CR))
+DEM_distance <- dplyr::filter(all_hypsos, Method == "DEM")
+non_DEM_distance <- dplyr::filter(all_hypsos, Method != "DEM")
+methods <- unique(non_DEM_distance$Method)
+Distances <- data.frame(DOW = NA, Method = NA, Mean_Absolute_Diff = NA, 
+                        Mean_DEM_Diff = NA, RMS_Diff = NA)
 
-for (i in seq_along(Perc_DOWs_CR)) {
+for (j in 1:length(methods)) {
   
-  ImageJ.hypso <- All_Hypsos %>% filter(DOW == Perc_DOWs_CR[i], Method == "ImageJ_CR")
-  DEM.hypso <- All_Hypsos %>% filter(DOW == Perc_DOWs_CR[i], Method == "DEM")
+  temp <- dplyr::filter(all_hypsos, Method == methods[j])
+  temp_dow <- unique(temp$DOW)
+  avg.abs.diff <- rep(NA, length(unique(temp$DOW)))
+  root.meansq.diff <- rep(NA, length(unique(temp$DOW)))
+  avg.DEM.diff <- rep(NA, length(unique(temp$DOW)))
   
-  max.depth <- round(max(ImageJ.hypso$depth_feet, DEM.hypso$depth_feet), digits = 1)
-  depth.vals <- seq(0, max.depth, .1)
+  for (i in seq_along(temp_dow)) {
+    
+    digitized.hypso <- temp %>% filter(DOW == temp_dow[i])
+    DEM.hypso <- DEM_distance %>% filter(DOW == temp_dow[i])
+    
+    max.depth <- round(max(digitized.hypso$depth_feet, DEM.hypso$depth_feet), digits = 1)
+    depth.vals <- seq(0, max.depth, .1)
+    
+    digitized.interp <- approx(digitized.hypso$depth_feet, digitized.hypso$proportion_area, 
+                            xout = depth.vals, yright = 0, yleft = 1)
+    DEM.interp <- approx(DEM.hypso$depth_feet, DEM.hypso$proportion_area, 
+                         xout = depth.vals, yright = 0, yleft = 1)
+    
+    # Avg absolute difference
+    avg.abs.diff[i] <- mean(abs(digitized.interp$y - DEM.interp$y))
+    # Root mean squared difference
+    root.meansq.diff[i] <- sqrt(mean((digitized.interp$y - DEM.interp$y)^2))
+    # Avg signed difference between ImageJ and DEM
+    avg.DEM.diff[i] <- mean(digitized.interp$y - DEM.interp$y)
+  } #first for loop
   
-  ImageJ.interp <- approx(ImageJ.hypso$depth_feet, ImageJ.hypso$proportion_area, 
-                          xout = depth.vals, yright = 0, yleft = 1)
-  DEM.interp <- approx(DEM.hypso$depth_feet, DEM.hypso$proportion_area, 
-                       xout = depth.vals, yright = 0, yleft = 1)
+  Distances_temp <- data.frame(DOW = temp_dow, Method = methods[j], Mean_Absolute_Diff = avg.abs.diff, 
+                             Mean_DEM_Diff = avg.DEM.diff, RMS_Diff = root.meansq.diff)
+  Distances_temp <- arrange(Distances_temp, DOW)
+  Distances <- rbind(Distances, Distances_temp)
   
-  # Avg absolute difference
-  avg.abs.diff[i] <- mean(abs(ImageJ.interp$y - DEM.interp$y))
-  # Root mean squared difference
-  root.meansq.diff[i] <- sqrt(mean((ImageJ.interp$y - DEM.interp$y)^2))
-  # Avg signed difference between ImageJ and DEM
-  avg.ImageJ.DEM.diff[i] <- mean(ImageJ.interp$y - DEM.interp$y)
-}
-
-Distances_CR <- data.frame(DOW = Perc_DOWs_CR, Mean_Absolute_Diff = avg.abs.diff, 
-                           Mean_ImageJ_DEM_Diff = avg.ImageJ.DEM.diff, RMSDiff = root.meansq.diff)
-Distances_CR <- arrange(Distances_CR, DOW)
-
-
-## Difference between hypsos Amanda did and DEM
-avg.abs.diff <- rep(NA, length(Perc_DOWs_AVP))
-root.meansq.diff <- rep(NA, length(Perc_DOWs_AVP))
-avg.ImageJ.DEM.diff <- rep(NA, length(Perc_DOWs_AVP))
-
-for (i in seq_along(Perc_DOWs_AVP)) {
-  
-  ImageJ.hypso <- All_Hypsos %>% filter(DOW == Perc_DOWs_AVP[i], Method == "ImageJ_AVP")
-  DEM.hypso <- All_Hypsos %>% filter(DOW == Perc_DOWs_AVP[i], Method == "DEM")
-  
-  max.depth <- round(max(ImageJ.hypso$depth_feet, DEM.hypso$depth_feet), digits = 1)
-  depth.vals <- seq(0, max.depth, .1)
-  
-  ImageJ.interp <- approx(ImageJ.hypso$depth_feet, ImageJ.hypso$proportion_area, 
-                          xout = depth.vals, yright = 0, yleft = 1)
-  DEM.interp <- approx(DEM.hypso$depth_feet, DEM.hypso$proportion_area, 
-                       xout = depth.vals, yright = 0, yleft = 1)
-  
-  # Avg absolute difference
-  avg.abs.diff[i] <- mean(abs(ImageJ.interp$y - DEM.interp$y))
-  # Root mean squared difference
-  root.meansq.diff[i] <- sqrt(mean((ImageJ.interp$y - DEM.interp$y)^2))
-  # Avg signed difference between ImageJ and DEM
-  avg.ImageJ.DEM.diff[i] <- mean(ImageJ.interp$y - DEM.interp$y)
-}
-
-Distances_AVP <- data.frame(DOW = Perc_DOWs_AVP, Mean_Absolute_Diff = avg.abs.diff, 
-                            Mean_ImageJ_DEM_Diff = avg.ImageJ.DEM.diff, RMSDiff = root.meansq.diff)
-Distances_AVP <- arrange(Distances_AVP, DOW)
-
+} #second for loop
+Distances <- na.omit(Distances)
 
 
 ## Difference between hypsos Amanda and Chris did
@@ -197,53 +213,95 @@ Distances_AVP_CR <- data.frame(DOW = Perc_DOWs_AVP, Mean_Absolute_Diff = avg.abs
                             Mean_ImageJ_DEM_Diff = avg.ImageJ.DEM.diff, RMSDiff = root.meansq.diff)
 Distances_AVP_CR <- arrange(Distances_AVP_CR, DOW)
 
-# Once AVP finishes write csv the merge of all the distances df
 
+mosaic::mean(Mean_Absolute_Diff~Method, data = Distances)
+mean(Distances_AVP_CR$Mean_Absolute_Diff) #0.0157
 
-quantile(Distances_CR$Mean_Absolute_Diff, seq(0,1, .05))
+mosaic::median(Mean_Absolute_Diff~Method, data = Distances)
+median(Distances_AVP_CR$Mean_Absolute_Diff) # 0.0078
 
-mean(Distances_CR$Mean_Absolute_Diff) # .051
-mean(Distances_AVP$Mean_Absolute_Diff) # 0.047
-mean(Distances_AVP_CR$Mean_Absolute_Diff) #0.016
+mosaic::sd(Mean_Absolute_Diff~Method, data = Distances)
 
-median(Distances_CR$Mean_Absolute_Diff) # .032
-median(Distances_AVP$Mean_Absolute_Diff)
-median(Distances_AVP_CR$Mean_Absolute_Diff)
-
-
-se(Distances_CR$Mean_Absolute_Diff) # .0055 # need se function
+Distances_CR <- Distances %>%
+  dplyr::filter(Method == "ImageJ_CR")
+Distances_AVP <- Distances %>%
+  dplyr::filter(Method == "ImageJ_AVP")
 
 t.test(Distances_CR$Mean_Absolute_Diff,   conf.level = 0.95)
-# Mean absolute distance is .051 (.040, .061)
+# Mean absolute distance is .051 (.040, .062)
 t.test(Distances_AVP$Mean_Absolute_Diff,   conf.level = 0.95)
-
-# Alternative: Bootstrap mean and CI
-samp.mean <- do(10000)*{mean(~Mean_Absolute_Diff, data = resample(Distances_CR))}
-head(samp.mean)
-(CIb <- qdata(~result, data = samp.mean, p = c(0.025, 0.975)))
-mean(~result, data = samp.mean)
-# .051 (.041, .062)
+# Mean absolute distance is .046 (.035, .057)
+t.test(Distances_AVP_CR$Mean_Absolute_Diff, conf.level = 0.95)
+# Mean absolute distance is .016 (.011, .021)
 
 sum(Distances_CR$Mean_Absolute_Diff > 0.10)/100 # 14% (14) greater than 10% difference on average
 sum(Distances_CR$Mean_Absolute_Diff > 0.15)/100 # 9% (9) greater than 15% difference on average
 
-Distances.df <- arrange(Distances_CR, desc(Mean_Absolute_Diff))
-head(Distances_CR, 10)
-par(mfrow = c(2,1))
-
-# Mean absolute differences
-hist(Distances_CR$Mean_Absolute_Diff, breaks = 20, xlim = c(0,.3), 
-     ylim = c(0,25), col = "blue", border = "white",  las = 1, xlab = "Mean absolute difference (proportion of lake area)",main = "")
-text(0.25, 22, "A", col = "red", cex = 2)
-
-# Average signed difference between ImageJ and DEM
-hist(Distances_CR$Mean_ImageJ_DEM_Diff, breaks = 20, xlim = c(-0.2,0.3), 
-     col = "blue", border = "white", las = 1, xlab = "Mean signed difference (proportion of lake area)", main = "")
-text(0.22, 22, "B", col = "red", cex = 2)
-mean(Distances_CR$Mean_ImageJ_DEM_Diff)
-t.test(Distances_CR$Mean_ImageJ_DEM_Diff, conf.level = 0.95)
 
 
+### ggplots ###
+
+# histogram MAD and MSD
+plot_df <- Distances %>%
+  dplyr::filter(Method == "ImageJ_AVP" | Method == "ImageJ_CR")
+ 
+#change legend attributes
+plot_df %>% ggplot(aes(Mean_Absolute_Diff, colour = Method)) +
+  geom_freqpoly() +
+  labs(y = "Frequency", x = "Mean Absolute Difference", colour = "Method")
+plot_df %>% ggplot(aes(Mean_DEM_Diff, colour = Method)) +
+  geom_freqpoly() +
+  labs(y = "Frequency", x = "Mean Signed Difference", colour = "Method")
+
+
+
+# good matches
+good_matches <- Distances_CR[ which(Distances_CR$Mean_Absolute_Diff <= 0.0152), ] 
+#arbitrary cutoff, chosen because it selects 16 lakes
+good_match_data <- subset(All_Hypsos, DOW %in% good_matches$DOW)
+
+windows()
+ggplot(good_match_data, aes(x = proportion_area, y = -1 * depth_feet, group = Method)) + 
+  #750 by 600 pixels works best
+  geom_line(aes(linetype = Method, col = Method)) +
+  geom_point(aes(shape = Method, col = Method)) + 
+  facet_wrap(~DOW, scales = 'free') +
+  labs(y = "Depth (feet)", x = "Proportion Area") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ggtitle("Hypsographic Curves With High Agreement")
+
+# poor matches
+example_match_data <- subset(All_Hypsos, DOW %in% c(71004600, 69004400, 38053900))
+unique(example_match_data$DOW)
+
+windows()
+ggplot(example_match_data, aes(x = proportion_area, y = -1 * depth_feet, group = Method)) + 
+  #750 by 600 pixels works best
+  geom_line(aes(linetype = Method, col = Method)) +
+  geom_point(aes(shape = Method, col = Method)) + 
+  facet_wrap(~DOW, scales = 'free') +
+  labs(y = "Depth (feet)", x = "Proportion Area") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  ggtitle("Hypsographic Curves With Low Agreement")
+
+lake_areas$DOW <- fixlakeid(lake_areas$DOW)
+area_diff = merge(Distances, lake_areas)
+max_depth = max_depth[,-3]
+area_diff = merge(area_diff, max_depth, by = 'DOW')
+
+
+#change legend attributes
+area_diff %>% ggplot(aes(x = max_depth, y = Mean_Absolute_Diff, col = Method), ylab("MAD"), xlab("Max depth (ft)")) +
+  geom_point() +
+  geom_smooth(method = "lm") + 
+  labs(y = "MAD", x = "Max depth (ft)") +
+  ggtitle("MAD vs lake depth seperated by method")
+
+area_diff %>% ggplot(aes(x = log(Area_acres), y = Mean_Absolute_Diff, col = Method), ylab("MAD"), xlab("Max depth (ft)")) +
+  geom_point() +
+  geom_smooth(method = "lm") + 
+  labs(y = "MAD", x = "Log Lake Area (Acres)") +
+  ggtitle("MAD vs lake size seperated by method")
 
 
 
@@ -282,26 +340,25 @@ WI_Hypsos <- WI_Hypsos %>% filter(!is.na(DOW)) # removes NA first row
 
 
 
-
-maxdepth <- data.frame("dow" = character(1), "max_depth" = numeric(1), stringsAsFactors = FALSE)
+maxdepth <- data.frame("DOW" = character(1), "max_depth" = numeric(1), stringsAsFactors = FALSE)
 Hypsos <- rbind(WI_Hypsos, MN_Hypsos)
 unique(Hypsos$DOW)
-max_depth = Hypsos[1,1]; dow = Hypsos[1,3]
+max_depth = Hypsos[1,1]; DOW = Hypsos[1,3]
 
 i = 1
 for (row in 1:nrow(Hypsos_depth)) {
-  if (dow != Hypsos[row,3]) {
-    maxdepth[i, 1] = dow
+  if (DOW != Hypsos[row,3]) {
+    maxdepth[i, 1] = DOW
     maxdepth[i, 2] = max_depth
     i = i + 1
-    dow = Hypsos[row,3]
+    DOW = Hypsos[row,3]
     max_depth = Hypsos[row, 1]
   }
   else {
     max_depth = Hypsos[row, 1]
   }
 }
-maxdepth[i,1] = dow; maxdepth[i,2] = max_depth
+maxdepth[i,1] = DOW; maxdepth[i,2] = max_depth
 
 # statistics for all lake depths
 mean(maxdepth$max_depth)  #31.8
